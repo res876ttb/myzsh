6243a260f76f34e58b6ed78d0f755a7fe54367b8
res876ttb <someone10851222@gmail.com>
2020-04-26 21:22:24 +0800

    Update: switch to zim and powerlevel10k

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
 #!/bin/bash -e
 
+which zsh
+
 # backup original tmux configuration
 mv $HOME/.zshrc $HOME/.zshrc.$(date "+%Y.%m.%d-%H:%M:%S")
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install oh-my-zsh plugin
-cd $HOME/.oh-my-zsh/custom/plugins/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
 # clone from github
 git clone https://github.com/res876ttb/myzsh.git ~/.myzsh
 cd ~/.myzsh
 
-# creaet symbolic link to $HOME
-mv $HOME/.zshrc $HOME/.zshrc.$(date "+%Y.%m.%d-%H:%M:%S").2
-ln -s $PWD/.zshrc $HOME/
+# Backup original script
+mv $HOME/.zshrc $HOME/.zshrc.$(date "+%Y.%m.%d-%H:%M:%S")
+mv $HOME/.p10k.zsh $HOME/.p10k.zsh.$(date "+%Y.%m.%d-%H:%M:%S")
+mv $HOME/.zimrc $HOME/.zimrc.$(date "+%Y.%m.%d-%H:%M:%S")
 
-# create link of custom theme to oh my zsh
-ln -s $HOME/.myzsh/ys.zsh-theme $HOME/.oh-my-zsh/custom/myys.zsh-theme
+# creaet symbolic link to $HOME
+ln -s $PWD/.zshrc $HOME/
+ln -s $PWD/.p10k.zsh $HOME/
+ln -s $PWD/.zimrc $HOME/
 
 touch $HOME/.zsh_profile
+
+# install powerlevel10k
+zsh $HOME/.zim/zimfw.zsh install
 
 exec zsh
