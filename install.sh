f4bf62de516729ec444327993874d6a60d0c8a3c
res876ttb <someone10851222@gmail.com>
2024-07-27 20:02:54 +0800

    Fix installation process

--- install.sh
+++ install.sh
@@ -1,45 +1,58 @@
 #!/bin/bash -e
 
-which zsh
+# show warnings
+cat << EOF
+Welcome using myzsh!
+This script will help you creating fancy zsh shell with auto completion features.
+Note that the following files will be replaced:
+.zshrc, .p10k.zsh, .zimrc, .zlogin
+Also, the .zshenv will be modified: being created or adding one line "skip_global_compinit=1"
+EOF
+read -p "Press enter to continue or ctrl+c to abort:"
 
-# backup original tmux configuration
-if [ -f $HOME/.zshrc ]; then
-  mv $HOME/.zshrc $HOME/.zshrc.$(date "+%Y.%m.%d-%H:%M:%S")
-fi
+# setup environments
+which zsh
+cwd="$(pwd)"
+
+export ZIM_HOME=$HOME/.zim
 
 rm -rf ~/.myzsh
 rm -rf ~/.zim
+rm -f ~/.zshrc
+rm -f ~/.p10k.zsh
+rm -f ~/.zimrc
+rm -f ~/.zlogin
 
-# install oh-my-zsh
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+# install zim without executing chsh... fool it by setting SHELL to zsh
+zsh -c "export SHELL=zsh; $(curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh)"
 
-# clone from github
+# clone myzsh
 git clone https://github.com/res876ttb/myzsh.git ~/.myzsh
 cd ~/.myzsh
 
-# Backup original script
-if [ -f $HOME/.zshrc ]; then
-  mv $HOME/.zshrc $HOME/.zshrc.$(date "+%Y.%m.%d-%H:%M:%S")
-fi
-if [ -f $HOME/.p10k.zsh ]; then
-  mv $HOME/.p10k.zsh $HOME/.p10k.zsh.$(date "+%Y.%m.%d-%H:%M:%S")
-fi
-if [ -f $HOME/.zimrc ]; then
-  mv $HOME/.zimrc $HOME/.zimrc.$(date "+%Y.%m.%d-%H:%M:%S")
-fi
-if [ -f $HOME/.zlogin ]; then
-  mv $HOME/.zlogin $HOME/.zlogin.$(date "+%Y.%m.%d-%H:%M:%S")
-fi
-
 # creaet symbolic link to $HOME
+rm -f ~/.zshrc
+rm -f ~/.zimrc
 ln -s $PWD/.zshrc $HOME/
 ln -s $PWD/.p10k.zsh $HOME/
 ln -s $PWD/.zimrc $HOME/
 ln -s $PWD/.zlogin $HOME/
 
+# create customized zsh file
 touch $HOME/.zsh_profile
 
 # install powerlevel10k
 zsh $HOME/.zim/zimfw.zsh install
 
+# Check whether we have skip_global_compinit in ~/.zshenv
+if [ -e "$HOME/.zshenv" ]; then
+	if [ "$(grep skip_global_compinit $HOME/.zshenv)" == "" ]; then
+		echo "skip_global_compinit=1" >> $HOME/.zshenv
+	fi
+else
+	echo "skip_global_compinit=1" > $HOME/.zshenv
+fi
+
+# Launch zsh
+cd $cwd
 exec zsh
